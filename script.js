e14a26320f0dfb00f2f1b0c25611d7d55c4e691b
nixus619 <nickdcedor@gmail.com>
2024-07-03 14:21:44 -0400

    Finished project
    Added the opacity idea that it will start out with a random opacity and then get darker every time a hover is done, until 100% opacity is reached. Added the resize and reset buttons with listener functions. Project should be good to be completed.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const container = document.querySelector('.container');
+const buttonDiv = document.querySelector(".buttons");
 //const div = document.createElement("div");
 //div.classList.add("square-element");
 //container.appendChild(div);
 //const another = document.createElement("div");
 //another.classList.add("square-element");
 //container.appendChild(another);
+const resize = document.createElement("button");
+resize.textContent = "Resize";
+resize.className="buttons"
+const clear = document.createElement("button");
+clear.textContent = "Reset";
+clear.className="buttons";
+buttonDiv.append(resize);
+buttonDiv.append(clear);
 
 function createGrid(number) {
     for(let i=0;i<number;i++){
@@ -24,10 +33,32 @@ function createGrid(number) {
             div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
         }*/
     }
+    const cells = document.querySelectorAll(".row");
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover",() => {
+            if(cell.style.background=="") {
+                const r = Math.floor(Math.random() * 256);
+                const g = Math.floor(Math.random() * 256);
+                const b = Math.floor(Math.random() * 256);
+                const opac = Math.random();
+                //console.log(opacity);
+                //console.log("rgb(" + r + "," + g + "," + b + " / " + opacity + "%)");
+                cell.style.background = "rgb(" + r + "," + g + "," + b + ")";
+                cell.style.opacity = opac;
+                } else {
+                    if(cell.style.opacity>1) {
+                        console.log("Max opacity reached");
+                    } else {
+                        console.log(cell.style.opacity);
+                        cell.style.opacity = parseFloat(cell.style.opacity) + 0.1;
+                    }
+                }
+        });
+    });
 }
 
 createGrid(16);
-const cells = document.querySelectorAll(".row");
+/* const cells = document.querySelectorAll(".row");
 cells.forEach((cell) => {
     cell.addEventListener("mouseover",() => {
         const r = Math.floor(Math.random() * 256);
@@ -35,4 +66,21 @@ cells.forEach((cell) => {
         const b = Math.floor(Math.random() * 256);
         cell.style.background = "rgb(" + r + "," + g + "," + b + ")";
     });
+}); */
+
+resize.addEventListener("click",function() {
+    let size = prompt("What size do you want the grid to be? Please select a value between 1 and 100");
+    while(size<1 || size>100){
+        size = prompt("Invalid value selected. Please select a value between 1 and 100");
+    }
+    container.innerHTML = '';
+    createGrid(size);
 });
+
+clear.addEventListener("click",function() {
+    const cells = document.querySelectorAll(".row");
+    cells.forEach((cell) => {
+        cell.style.background = "";
+        cell.style.opacity = 1;
+        });
+    });
